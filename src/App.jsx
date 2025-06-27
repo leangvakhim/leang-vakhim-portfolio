56c0cc09fe6a4554be094c5d90b57afe8403b11d
leangvakhim <Vakhiml60@gmail.com>
2025-06-27 16:36:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Contact from './Page/Contact'
 import { API_ENDPOINTS, axiosInstance } from './Component/APIConfig'
 
 function App() {
+  const [guestLoginSuccess, setGuestLoginSuccess] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loginGuest = async () => {
@@ -21,27 +23,37 @@ function App() {
         });
 
         sessionStorage.removeItem("guest_token");
+        setGuestLoginSuccess(true);
 
       } catch (error) {
         console.error("Login failed:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
     loginGuest();
   }, []);
 
+  useEffect(() => {
+    if (!guestLoginSuccess) return;
+
+  }, [guestLoginSuccess]);
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/about" />}/>
-        <Route path='/about' element={<About/>} />
-        <Route path='/resume' element={<Resume/>} />
-        <Route path='/portfolio' element={<Portfolio/>} />
-        <Route path='/blog' element={<Blog/>} />
-        <Route path='/contact' element={<Contact/>} />
+        <Route path="/" element={<Navigate to="/about" />} />
+        <Route path='/about' element={<About guestLoginSuccess={guestLoginSuccess} />} />
+        <Route path='/resume' element={<Resume guestLoginSuccess={guestLoginSuccess} />} />
+        <Route path='/portfolio' element={<Portfolio guestLoginSuccess={guestLoginSuccess} />} />
+        <Route path='/blog' element={<Blog guestLoginSuccess={guestLoginSuccess} />} />
+        <Route path='/contact' element={<Contact guestLoginSuccess={guestLoginSuccess} />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App
