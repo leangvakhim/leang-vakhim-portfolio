86c05acbc37a2a4bb6382b76b7461feadb1d95ed
leangvakhim <Vakhiml60@gmail.com>
2025-07-01 20:23:45 +0700

    Update APIConfig.jsx

--- src/Component/APIConfig.jsx
+++ src/Component/APIConfig.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import axios from "axios";
 
-// const API = "https://api.aimostore.shop";
-const API = "http://127.0.0.1:8000";
+const API = "https://api.aimostore.shop";
+// const API = "http://127.0.0.1:8000";
 
 const API_BASEURL = `${API}/api`;
 
